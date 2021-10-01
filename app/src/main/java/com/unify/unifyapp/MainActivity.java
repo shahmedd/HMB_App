0eec7aa17f4b10f9ea89c062b6ddb63057b174a2
vaqasyounus <waqas.m@avanzasolutions.com>
2021-10-01 11:40:40 +0500

    - keyboard type - anyline sdk integrated partialy -

--- app/src/main/java/com/unify/unifyapp/MainActivity.java
+++ app/src/main/java/com/unify/unifyapp/MainActivity.java
@@ -2,11 +2,17 @@ package com.unify.unifyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.unify.avanza.Unify;
+import com.unify.avanza.anyline.IUnifyAnyLine;
+import com.unify.unifyapp.anyline.ScanMrzActivity;
 
-public class MainActivity extends AppCompatActivity {
+import at.nineyards.anyline.core.LicenseException;
+import io.anyline.AnylineSDK;
+
+public class MainActivity extends AppCompatActivity implements IUnifyAnyLine {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Bundle bundle = new Bundle();
         bundle.putString("url", "http://10.0.2.2:3000/unify_json");
-        Unify.getInstance().initialize(this, null, bundle);
+        Unify.getInstance().initialize(this, this, bundle);
+
+    }
+
+    @Override
+    public void onStartScanning() {
+        try {
+            AnylineSDK.init(getString(R.string.anyline_license_key), this);
+        } catch (LicenseException e) {
+
+        }
+        startActivity(new Intent(this, ScanMrzActivity.class));
+    }
+
+    @Override
+    public void onConfirmClick() {
+
     }
 }
