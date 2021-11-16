a19b51e03bc8df1b5676de46a54b2d7fcb0dc71c
vaqasyounus <waqas.m@avanzasolutions.com>
2021-11-16 16:11:20 +0500

    - imageButton on click to update inappmemory - main activity changes

--- app/src/main/java/com/unify/unifyapp/MainActivity.kt
+++ app/src/main/java/com/unify/unifyapp/MainActivity.kt
@@ -2,32 +2,24 @@ package com.unify.unifyapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.unify.avanza.anyline.IDocumentScanner
 import com.unify.avanza.app.Unify
 import com.unify.avanza.services.network.IUnifyInterceptor
 import com.unify.avanza.services.network.IUnifyNetworkService
 import java.util.*
 import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity(), IDocumentScanner, IUnifyNetworkService {
+class MainActivity : AppCompatActivity(), IUnifyNetworkService {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bundle = Bundle()
 //        bundle.putString("url", "http://10.0.2.2:3000/unify_json")
-//        bundle.putString("url", "http://192.168.100.158:3000/unify_json")
+//        bundle.putString("url", "http://192.168.100.202:3000/unify_json")
 //        bundle.putString("url", "https://run.mocky.io/v3/f1a00d07-1a9f-4da9-a65d-e74d072b14ef")
-        bundle.putString("url", "http://mdu-site1.avanzasolutions.com:8010/unifyjsons/presalesDemo.json")
-        Unify.getInstance().initialize(this, null, this, this, bundle)
+        bundle.putString("url", "http://mdu-site1.avanzasolutions.com:8010/unifyjsons/eKYC.json")
+        Unify.getInstance().initialize(this, null, this, bundle)
     }
 
-    override fun onStartPassportScanning() {
-
-    }
-
-    override fun onStartUniversalIdScanning() {
-
-    }
 
     override fun addInterceptors(): List<Map<String, IUnifyInterceptor>> {
         val interceptors: MutableList<Map<String, IUnifyInterceptor>> = ArrayList()
