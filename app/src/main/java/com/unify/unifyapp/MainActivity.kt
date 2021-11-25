bf1da7463527c7e2b43f638c111edf6358dca5c2
vaqasyounus <waqas.m@avanzasolutions.com>
2021-11-25 14:41:59 +0500

    - anyline custom - trigger fixes - appbar flow changed in

--- app/src/main/java/com/unify/unifyapp/MainActivity.kt
+++ app/src/main/java/com/unify/unifyapp/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity(), IUnifyNetworkService {
         setContentView(R.layout.activity_main)
         val bundle = Bundle()
 //        bundle.putString("url", "http://10.0.2.2:3000/unify_json")
-//        bundle.putString("url", "http://192.168.100.202:3000/unify_json")
-//        bundle.putString("url", "https://run.mocky.io/v3/f1a00d07-1a9f-4da9-a65d-e74d072b14ef")
         bundle.putString("url", "http://mdu-site1.avanzasolutions.com:8010/unifyjsons/eKYC.json")
         Unify.getInstance().initialize(this, null, this, bundle)
     }
