1551ad3bc63186c114683ab33026471020291cc8
vaqasyounus <waqas.m@avanzasolutions.com>
2021-12-08 15:55:44 +0500

    - updateMainComponentId trigger developed in StorageService - host application register any binding key to get data from sdk

--- app/src/main/java/com/unify/unifyapp/MainActivity.kt
+++ app/src/main/java/com/unify/unifyapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.unify.unifyapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.unify.avanza.EventKeyValue
 import com.unify.avanza.app.Unify
 import com.unify.avanza.services.network.IUnifyInterceptor
 import com.unify.avanza.services.network.IUnifyNetworkService
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -16,6 +21,9 @@ class MainActivity : AppCompatActivity(), IUnifyNetworkService {
 //        bundle.putString("url", "http://10.0.2.2:3000/unify_json")
         bundle.putString("url", "http://mdu-site1.avanzasolutions.com:8010/unifyjsons/eKYC.json")
         Unify.getInstance().initialize(this, null, this, bundle)
+        EventBus.getDefault().register(this)
+
+
     }
 
 
@@ -26,5 +34,15 @@ class MainActivity : AppCompatActivity(), IUnifyNetworkService {
         aesInter["checksum"] = CheckSumInterceptor
         interceptors.add(aesInter)
         return interceptors
+    }
+
+    override fun getValueAgainstBindingKey(): List<String> {
+        return listOf("usCitizenSwitch")
+
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: EventKeyValue) {
+        Log.d("Received in host app", event.bindingKey + " : " + event.value)
     }
 }
