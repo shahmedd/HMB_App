4f949dfb8d1f8e3749c33b268aec65b72cd94b8c
vaqasyounus <waqas.m@avanzasolutions.com>
2022-03-15 12:13:33 +0500

    - main activity ui changed

--- app/src/main/java/com/unify/unifyapp/MainActivity.kt
+++ app/src/main/java/com/unify/unifyapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.unify.unifyapp
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.unify.avanza.EventKeyValue
 import com.unify.avanza.UnifyRuntimePermission
@@ -14,6 +15,8 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
 class MainActivity : AppCompatActivity(), IUnifyNetworkService {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,7 +34,7 @@ class MainActivity : AppCompatActivity(), IUnifyNetworkService {
     }
 
     override fun getValueAgainstBindingKey(): List<String> {
-        return listOf("expire")
+        return listOf("expire", "hideProgress")
 
     }
 
@@ -42,25 +45,40 @@ class MainActivity : AppCompatActivity(), IUnifyNetworkService {
 
 
     fun initializeSDK(view: View) {
-        UnifyRuntimePermission.getPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, object : UnifyRuntimePermission.UnifyPermissionGrantedResponse {
-            override fun onUnifyPermissionGranted() {
-                val dataFromHostapp: MutableMap<String, Any> = HashMap();
-                dataFromHostapp["CPR"] = "00700" // this value provide by host app
-//            Unify.getInstance().initializeSDK(this, "http://10.0.2.2:3000/unify_json", dataFromHostapp, null, this)
-//            Unify.getInstance().initializeSDK(this, "http://172.16.2.229:3000/unify_json", dataFromHostapp, null, this)
-                Unify.getInstance().initializeSDK(this@MainActivity, "http://mdu-site1.avanzasolutions.com:8010/unifyjsons/presales_new_demo.json", dataFromHostapp, null, this@MainActivity)
-            }
+        var editText = findViewById<EditText>(R.id.url)
 
-        })
+        UnifyRuntimePermission.getPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            object : UnifyRuntimePermission.UnifyPermissionGrantedResponse {
+                override fun onUnifyPermissionGranted() {
+                    val dataFromHostapp: MutableMap<String, Any> = HashMap();
+                    dataFromHostapp["CPR"] = "00700" // this value provide by host app
+                    Unify.getInstance().initializeSDK(this@MainActivity,
+                        editText.text.toString(),
+                        true,
+                        dataFromHostapp,
+                        null,
+                        this@MainActivity)
+                    /*Unify.getInstance().initializeSDK(this@MainActivity,
+                        "http://mdu-site1.avanzasolutions.com:8010/unifyjsons/Pre_Sales_Onboarding_Master.json",
+                        true,
+                        dataFromHostapp,
+                        null,
+                        this@MainActivity)*/
+                }
+
+            })
     }
 
     fun launchSDK(view: View) {
-        UnifyRuntimePermission.getPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, object : UnifyRuntimePermission.UnifyPermissionGrantedResponse {
-            override fun onUnifyPermissionGranted() {
-                Unify.getInstance().launchSDK(this@MainActivity, null);
-            }
+        UnifyRuntimePermission.getPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            object : UnifyRuntimePermission.UnifyPermissionGrantedResponse {
+                override fun onUnifyPermissionGranted() {
+                    Unify.getInstance().launchSDK(this@MainActivity, null);
+                }
 
-        })
+            })
 
     }
 
